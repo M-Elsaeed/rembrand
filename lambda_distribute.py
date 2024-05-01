a285b08dce51220c133d7e72b1fe4ba164cc62f6
Mohammed Ehab ElSaeed <m.elsaeed1998@gmail.com>
2024-05-01 21:46:05 +0300

    Only Distribute Lambda

--- lambda_distribute.py
+++ lambda_distribute.py
@@ -1,7 +1,5 @@
-import boto3
 import json
 import uuid
-import time
 
 def lambda_handler(event, context):
     print(event)
@@ -22,58 +20,9 @@ def lambda_handler(event, context):
         segment_end = segment_start + segment_length if i < segments - 1 else end        
         tasks_parameters.append({
             'url': url,
-            'start': segment_start,
-            'end': segment_end,
+            'start': str(segment_start),
+            'end': str(segment_end),
             "job_id": job_id
         })
-
-    print(tasks_parameters)
-
-    ecs_client = boto3.client('ecs')
     
-    cluster = 'arn:aws:ecs:eu-north-1:663539659680:cluster/ECS_Rendering_Cluster'
-    task_definition = 'arn:aws:ecs:eu-north-1:663539659680:task-definition/Blender_Render_And_Upload:3'
-
-    for task_params in tasks_parameters:
-        retries = 0
-        while retries < 5:
-            response = ecs_client.run_task(
-                cluster=cluster,
-                launchType='EC2',
-                taskDefinition=task_definition,
-                overrides={
-                    'containerOverrides': [
-                        {
-                            'name': "render_and_upload",
-                            'environment': [
-                                {'name': 'URL', 'value': task_params['url']},
-                                {'name': 'START', 'value': str(task_params['start'])},
-                                {'name': 'END', 'value': str(task_params['end'])},
-                                {'name': 'JOB_ID', 'value': task_params['job_id']}
-                            ]
-                        }
-                    ]
-                }
-                )
-
-            print(task_params['start'])
-            print(len(response['failures']))
-            print(response)
-            if len(response['failures']) == 0:
-                retries += 10
-            else:
-                time.sleep(5)
-                retries += 1
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps(f'ECS tasks invoked successfully. job_id: { job_id}')
-    }
-
-# Test the lambda function
-# lambda_handler({"body":{
-#     'blender_url': 'https://drive.google.com/uc?export=download&id=14ZCrRigm_7PI9u6j_QXSrNzj6AkwJiCs',
-#     'blender_start': 60,
-#     'blender_end': 70,
-#     'blender_segments': 5
-# }}, None)
+    return tasks_parameters
